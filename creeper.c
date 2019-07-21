67a4007bf0c58ab580f86c4f0ed29ad68a25404c
MyPoZi <kxx555u.u@gmail.com>
2019-07-21 12:25:03 +0900

    [add] 隠面消去と証明とシェーディングを追加

--- creeper.c
+++ creeper.c
@@ -1,21 +1,49 @@
-/*  p4-RotateTeaPot.c
- *  Rotating tea pot
- */
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <math.h>
 
 unsigned char mouseFlag = GL_FALSE;
-double	 theta = 0.0;						// angular of tea pot
+double	 theta = 0.0;
 int Start;
 double angle = 0.0;
 
 double	sizeOfTeapot = 1.0;
 
+float light_pos[] = { 10, 10, 10, 10 };
+
+float mtrl_diffuse[] = { 0.6, 0.6, 0.6, 0.0 };
+float mtrl_specular[] = { 0.0, 1.0, 0.3, 0.0 };
+float mtrl_shininess[] = { 0.8 };					// range [0,128]
+
+
 
 void myKeyboard( unsigned char key, int x, int y )
 {
     if ( key == 27 ) exit( 0 );
+}
+
+void mySetLight()
+{
+	//float light0_position[] = { 1.0,  1.0, 1.0, 1.0 };
+
+	/* LIGHT0 uses the default parameters except position */
+	//glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+	//glEnable(GL_LIGHT0);
+	/* Shading the both sides of box */
+	//glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+	GLfloat light_diffuse[] = { 0.9, 0.9, 0.9, 1.0 };	// 拡散反射光
+	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };	// 鏡面反射光
+	GLfloat light_ambient[] = { 0.5, 1.0, 1.0, 0.6 };	// 環境光
+	GLfloat light_position[] = { -0.5, 1.0, 1.0, 1.0 };	// 位置と種類
+
+	// 光源の設定
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);	 // 拡散反射光の設定
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular); // 鏡面反射光の設定
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);	 // 環境光の設定
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position); // 位置と種類の設定
+
+	glShadeModel(GL_SMOOTH);	// シェーディングの種類の設定
+	glEnable(GL_LIGHT0);		// 光源の有効化
 }
 
 void myFloor()
@@ -45,7 +73,26 @@ void myFloor()
 void myDisplay(void)
 {
 
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mtrl_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mtrl_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mtrl_shininess);
+
+	// glPushMatrix();
+
+	//glClearColor(0.2f, 0.2f, 0.2f, 1.0);					// 背景色
+	glMatrixMode(GL_MODELVIEW);
+
+	glEnable(GL_DEPTH_TEST);		// 隠面処理の適用
+	mySetLight();					// 光源の設定
+	glEnable(GL_LIGHTING);		// 光源ON
+	// glPopMatrix();
+
+
+
+
+
 	glPushMatrix();
 		glTranslated(0.0, -3.0, 0.0);
 		myFloor();
@@ -58,16 +105,20 @@ void myDisplay(void)
 
 	glPushMatrix();
 
+
+
+
+
+
 	// head
 	glPushMatrix();
-		glColor3d(1.0, 1.0, 1.0);
-		glutWireCube(1.0);
-		//glutWireTeapot(1.0);
+		glColor3d(0.0, 1.0, 0.0);
+		glutSolidCube(1.0);
 	glPopMatrix();
 	// body
 	glTranslated(0.0, -1.5, 0.0);
 	glPushMatrix();
-		glColor3d(1.0, 1.0, 1.0);
+		glColor3d(0.0, 1.0, 0.0);
 		glScaled(0.9, 2.0, 0.5);
 		glutSolidCube(1.0);
 	glPopMatrix();
@@ -77,23 +128,26 @@ void myDisplay(void)
 		glColor3d(1.0, 1.0, 1.0);
 		glTranslated(0.0, 0.0, 0.5);
 		glScaled(0.9, 1.0, 0.5);
-		glutWireCube(1.0);
+		glutSolidCube(1.0);
 	glPopMatrix();
 	// foot2
 		glPushMatrix();
 		glColor3d(1.0, 1.0, 1.0);
 		glTranslated(0.0, 0.0, -0.5);
 		glScaled(0.9, 1.0, 0.5);
-		glutWireCube(1.0);
+		glutSolidCube(1.0);
 	glPopMatrix();
 
 
 	glPopMatrix();
 
 
-    glutSwapBuffers(); 
+
+    glutSwapBuffers();
+
 
 }
+
 
 void myMouseMotion(int x, int y)
 {
@@ -147,7 +201,7 @@ void mySetMenu()
 
 void myIdle(void)
 {
-	theta = fmod(theta + 0.001, 360.0);
+	theta = fmod(theta + 0.01, 360.0);
 	glutPostRedisplay();
 }
 
@@ -159,7 +213,7 @@ void myInit (char *progname)
 	glutInitWindowSize( width, height);
 	glutInitWindowPosition(0, 0); 
 	glutCreateWindow(progname);
-	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0);					// 背景色
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(90.0, (double)width/(double)height, 0.1, 20.0);
@@ -176,6 +230,7 @@ int main(int argc, char** argv)
 	glutMouseFunc(myMouseFunc);
 	glutMotionFunc(myMouseMotion);
 	mySetMenu();
+	mySetLight();
 	glutIdleFunc(myIdle);
 	glutDisplayFunc(myDisplay);
 	glutMainLoop(); 
